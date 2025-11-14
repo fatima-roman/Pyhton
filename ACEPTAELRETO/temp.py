1924a386817979fc81dede2f947ab6d8150d3040
fatima-roman <fatima.roman@iesnervion.es>
2025-11-14 12:24:59 +0100

    784

--- ACEPTAELRETO/temp.py
+++ ACEPTAELRETO/temp.py
@@ -4,15 +4,9 @@ nAbreviatras = int(input())
 
 for i in range(nAbreviatras):
     abreviaturas=input()#.split() #b.,s.,d.
-    print(abreviaturas)
+    #print(abreviaturas)
 
-    for j in ((abreviaturas)):
-        if j != '.' and j != ' ':
-            solucion.append(2*j + '. ')
-
-
-for k in range(len(solucion)):
-    if k == len(solucion)-1:
-        print(solucion[k])
-    else:
-        print(solucion[k], end='')
+    for k in (abreviaturas):
+        if k != '.' and k != ' ':
+            print(2*k + '. ', end='')
+    print()
